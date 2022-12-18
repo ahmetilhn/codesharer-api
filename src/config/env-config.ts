7128496f926dbdcb3b5f49c37b327d958b860d0c
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-12-18 15:13:03 +0300

    🌍 envConfig created

---
+++ src/config/env-config.ts
@@ -0,0 +1,12 @@
+
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  API_PORT: process.env.API_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_CLUSTER: process.env.DB_CLUSTER,
+  DB_NAME: process.env.DB_NAME,
+};
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express";
+import envConfig from "./config/env-config";
+class Server {
+  app: Application = express();
+  listen() {
+    this.app.listen(envConfig.API_PORT, () => {
+      console.log(` Server is running on port ${envConfig.API_PORT}`);
+    });
+  }
+  init() {
+    this.listen();
+  }
+}
